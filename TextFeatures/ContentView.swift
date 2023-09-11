bfad94f143dc4ffc11ceaf6376704d6313083a9a
Elaidzha1940 <eli080293@gmail.com>
2023-09-11 17:46:48 +0300

    TextFeatures + added LinearGradient

--- TextFeatures/ContentView.swift
+++ TextFeatures/ContentView.swift
@@ -15,8 +15,16 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-           
+           LinearGradient(
+            colors: [.gray, .white, .green],
+            startPoint: .bottomLeading,
+            endPoint: .bottomTrailing)
             
+        }
+        .frame(height: 60)
+        .mask {
+            Text("elid.ev")
+                .font(.system(size: 90, weight: .bold, design: .serif))
         }
         .preferredColorScheme(.dark)
     }
