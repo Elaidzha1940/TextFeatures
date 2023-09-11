abda5085b17215b2b36f58aef559f5692ab6aefa
Elaidzha1940 <eli080293@gmail.com>
2023-09-11 17:37:35 +0300

    TextFeatures

---
+++ TextFeatures/ContentView.swift
@@ -0,0 +1,29 @@
+//  /*
+//
+//  Project: TextFeatures
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.09.2023
+//
+//  Status: #Complete | #Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+            
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextFeatures/TextFeaturesApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: TextFeatures
+//  File: TextFeaturesApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.09.2023
+//
+//  Status: #Complete | #Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct TextFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
